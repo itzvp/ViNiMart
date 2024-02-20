0c620c1a38bd778c2a5894b3f4cced0ba54e5d6e
itzvp <125799424+itzvp@users.noreply.github.com>
2024-02-21 02:46:09 +0530

    Worked on landing page

---
+++ src/app/(pages)/(private)/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Rating,
+  Stack,
+  Typography,
+} from "@mui/material";
+
+const HomeHero = () => {
+  return (
+    <Box
+      sx={{
+        pt: "120px",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={3} direction="column" alignItems="center">
+          <Grid item xs={12}>
+            <Typography variant="h2" align="center" sx={{ mb: 2 }}>
+              Let us worry about the&nbsp;
+              <Typography
+                component="span"
+                color="primary.main"
+                variant="inherit"
+              >
+                User Experience
+              </Typography>
+              , you focus on Developing.
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography
+              color="text.secondary"
+              align="center"
+              sx={{
+                fontSize: 20,
+                fontWeight: 500,
+              }}
+            >
+              A professional kit that comes with ready-to-use MUI components
+              developed with one common goal in mind, help you build faster &
+              beautiful.
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Stack
+              alignItems="center"
+              direction="row"
+              flexWrap="wrap"
+              spacing={1}
+              sx={{ my: 3 }}
+            >
+              <Rating readOnly value={4.7} precision={0.1} max={5} />
+              <Typography
+                color="text.primary"
+                variant="caption"
+                sx={{ fontWeight: 700 }}
+              >
+                4.7/5
+              </Typography>
+              <Typography color="text.secondary" variant="caption">
+                based on (70+ reviews)
+              </Typography>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+export default HomeHero;
