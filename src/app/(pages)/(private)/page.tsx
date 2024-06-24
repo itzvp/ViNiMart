0566a284dc269f79accdc8df6c8ba0777f3c1f56
itzvp <125799424+itzvp@users.noreply.github.com>
2024-06-24 16:37:23 +0530

    useTheme

--- src/app/(pages)/(private)/page.tsx
+++ src/app/(pages)/(private)/page.tsx
@@ -1,36 +1,25 @@
 "use client";
 import { Box } from "@mui/material";
 import TopNav from "./top-nav";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HeroSection from "@/components/HeroSection";
 import About from "@/components/About";
 import Services from "@/components/Services";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+
 const HomeHero = () => {
   return (
     <div>
       <TopNav />
-
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <HeroSection />
-                <About />
-                <Services />
-                <Contact />
-              </>
-            }
-          />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Box component="main">
+        <HeroSection />
+        <About />
+        <Services />
+        <Contact />
+      </Box>
+      <Footer />
     </div>
   );
 };
+
 export default HomeHero;
--- src/app/(pages)/(private)/top-nav-item.tsx
+++ src/app/(pages)/(private)/top-nav-item.tsx
@@ -1,3 +1,164 @@
+// import type { FC, ReactNode } from "react";
+// import { useCallback, useState } from "react";
+// import PropTypes from "prop-types";
+// import { alpha } from "@mui/system/colorManipulator";
+// import Box from "@mui/material/Box";
+// import ButtonBase from "@mui/material/ButtonBase";
+// import Paper from "@mui/material/Paper";
+// import Portal from "@mui/material/Portal";
+// import Typography from "@mui/material/Typography";
+
+// const TOP_NAV_HEIGHT = 64;
+// const TOP_NAV_SPACE = 16;
+// const OFFSET = 16;
+
+// interface TopNavItemProps {
+//   active?: boolean;
+//   external?: boolean;
+//   path?: string;
+//   popover?: ReactNode;
+//   title: string;
+// }
+
+// const TopNavItem: FC<TopNavItemProps> = (props) => {
+//   const { active, external, path, popover, title } = props;
+//   const [open, setOpen] = useState<boolean>(false);
+
+//   const handleMouseEnter = useCallback(() => {
+//     setOpen(true);
+//   }, []);
+
+//   const handleMouseLeave = useCallback(() => {
+//     setOpen(false);
+//   }, []);
+
+//   if (popover) {
+//     return (
+//       <>
+//         <Box
+//           component="li"
+//           sx={{
+//             display: "flex",
+//             alignItems: "center",
+//             height: "100%",
+//           }}
+//           onMouseEnter={handleMouseEnter}
+//         >
+//           <ButtonBase
+//             disableRipple
+//             sx={{
+//               alignItems: "center",
+//               borderRadius: 1,
+//               display: "flex",
+//               justifyContent: "flex-start",
+//               px: "16px",
+//               py: "8px",
+//               textAlign: "left",
+//               "&:hover": {
+//                 backgroundColor: "action.hover",
+//               },
+//               ...(active && {
+//                 color: "primary.main",
+//               }),
+//             }}
+//           >
+//             <Typography component="span" variant="subtitle2">
+//               {title}
+//             </Typography>
+//           </ButtonBase>
+//         </Box>
+//         {open && (
+//           <Portal>
+//             <Box
+//               onMouseEnter={handleMouseEnter}
+//               onMouseLeave={handleMouseLeave}
+//               sx={{
+//                 left: 0,
+//                 position: "fixed",
+//                 pt: `${OFFSET}px`,
+//                 right: 0,
+//                 top: TOP_NAV_HEIGHT + TOP_NAV_SPACE,
+//                 zIndex: (theme) => theme.zIndex.appBar + 100,
+//               }}
+//             >
+//               <Paper
+//                 elevation={16}
+//                 sx={{
+//                   backgroundColor: (theme) =>
+//                     alpha(theme.palette.background.paper, 0.9),
+//                   backdropFilter: "blur(6px)",
+//                   mx: "auto",
+//                   width: (theme) => theme.breakpoints.values.md,
+//                 }}
+//               >
+//                 {popover}
+//               </Paper>
+//             </Box>
+//           </Portal>
+//         )}
+//       </>
+//     );
+//   }
+
+//   const linkProps = path
+//     ? external
+//       ? {
+//           component: "a",
+//           href: path,
+//           target: "_blank",
+//         }
+//       : {
+//           href: path,
+//         }
+//     : {};
+
+//   return (
+//     <Box
+//       component="li"
+//       sx={{
+//         display: "flex",
+//         alignItems: "center",
+//         height: "100%",
+//       }}
+//     >
+//       <ButtonBase
+//         disableRipple
+//         sx={{
+//           alignItems: "center",
+//           borderRadius: 1,
+//           display: "flex",
+//           justifyContent: "flex-start",
+//           px: "16px",
+//           py: "8px",
+//           textAlign: "left",
+//           "&:hover": {
+//             backgroundColor: "action.hover",
+//           },
+//           ...(active && {
+//             color: "primary.main",
+//           }),
+//         }}
+//         {...linkProps}
+//       >
+//         <Typography component="span" variant="subtitle2">
+//           {title}
+//         </Typography>
+//       </ButtonBase>
+//     </Box>
+//   );
+// };
+
+// TopNavItem.propTypes = {
+//   active: PropTypes.bool,
+//   external: PropTypes.bool,
+//   path: PropTypes.string,
+//   // eslint-disable-next-line react/forbid-prop-types
+//   popover: PropTypes.any,
+//   title: PropTypes.string.isRequired,
+// };
+
+// export default TopNavItem;
+
 import type { FC, ReactNode } from "react";
 import { useCallback, useState } from "react";
 import PropTypes from "prop-types";
@@ -7,6 +168,7 @@ import ButtonBase from "@mui/material/ButtonBase";
 import Paper from "@mui/material/Paper";
 import Portal from "@mui/material/Portal";
 import Typography from "@mui/material/Typography";
+import Link from "next/link";
 
 const TOP_NAV_HEIGHT = 64;
 const TOP_NAV_SPACE = 16;
@@ -121,29 +283,31 @@ const TopNavItem: FC<TopNavItemProps> = (props) => {
         height: "100%",
       }}
     >
-      <ButtonBase
-        disableRipple
-        sx={{
-          alignItems: "center",
-          borderRadius: 1,
-          display: "flex",
-          justifyContent: "flex-start",
-          px: "16px",
-          py: "8px",
-          textAlign: "left",
-          "&:hover": {
-            backgroundColor: "action.hover",
-          },
-          ...(active && {
-            color: "primary.main",
-          }),
-        }}
-        {...linkProps}
-      >
-        <Typography component="span" variant="subtitle2">
-          {title}
-        </Typography>
-      </ButtonBase>
+      <Link href={path ?? "#"} passHref>
+        <ButtonBase
+          disableRipple
+          sx={{
+            alignItems: "center",
+            borderRadius: 1,
+            display: "flex",
+            justifyContent: "flex-start",
+            px: "16px",
+            py: "8px",
+            textAlign: "left",
+            "&:hover": {
+              backgroundColor: "action.hover",
+            },
+            ...(active && {
+              color: "primary.main",
+            }),
+          }}
+          {...linkProps}
+        >
+          <Typography component="span" variant="subtitle2">
+            {title}
+          </Typography>
+        </ButtonBase>
+      </Link>
     </Box>
   );
 };
@@ -152,7 +316,6 @@ TopNavItem.propTypes = {
   active: PropTypes.bool,
   external: PropTypes.bool,
   path: PropTypes.string,
-  // eslint-disable-next-line react/forbid-prop-types
   popover: PropTypes.any,
   title: PropTypes.string.isRequired,
 };
